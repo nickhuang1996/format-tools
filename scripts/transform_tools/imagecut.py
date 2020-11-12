a6094feebaf5f6c431ac42f9d1f52b8df1c74366
笳倞 黄 <nickhuang1996@126.com>
2020-11-12 19:55:16 +0800

    Update ImageCut function

--- scripts/transform_tools/imagecut.py
+++ scripts/transform_tools/imagecut.py
@@ -1,9 +1,7 @@
 import cv2
-import PIL
-from PIL import Image, ImageGrab
 import inspect
-import time
 import numpy as np
+import math
 
 
 def imagecut(ProcessBar, image_path, save_path, new_frame):
@@ -13,7 +11,12 @@ def imagecut(ProcessBar, image_path, save_path, new_frame):
     img_np = cv2.imread(image_path, 1)
 
     cropped = img_np[new_frame[2]: new_frame[3], new_frame[0]: new_frame[1]]
-    cv2.imshow("new", cropped)
+    new_height = cropped.shape[0]
+    new_width = cropped.shape[1]
+    resized_new_height = math.floor(new_height * 0.5)
+    resized_new_width = math.floor(new_width * 0.5)
+    resized_cropped = cv2.resize(cropped, (resized_new_width, resized_new_height))
+    cv2.imshow("new", resized_cropped)
     cv2.imwrite(save_path, cropped)
 
 
