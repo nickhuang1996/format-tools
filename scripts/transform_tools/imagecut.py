45a795b1faf4eb5baff9a7ea20682dfea1eaf3c7
笳倞 黄 <nickhuang1996@126.com>
2020-11-12 20:20:36 +0800

    Update ImageCut function

--- scripts/transform_tools/imagecut.py
+++ scripts/transform_tools/imagecut.py
@@ -2,13 +2,14 @@ import cv2
 import inspect
 import numpy as np
 import math
+import os
 
 
 def imagecut(ProcessBar, image_path, save_path, new_frame):
     """set the func name on pbar text"""
     ProcessBar.set_Text(inspect.stack()[0][3])
-
-    img_np = cv2.imread(image_path, 1)
+    """Solver Chinese path"""
+    img_np = cv2.imdecode(np.fromfile(image_path, dtype=np.uint8), -1)
 
     cropped = img_np[new_frame[2]: new_frame[3], new_frame[0]: new_frame[1]]
     new_height = cropped.shape[0]
@@ -16,7 +17,11 @@ def imagecut(ProcessBar, image_path, save_path, new_frame):
     resized_new_height = math.floor(new_height * 0.5)
     resized_new_width = math.floor(new_width * 0.5)
     resized_cropped = cv2.resize(cropped, (resized_new_width, resized_new_height))
-    cv2.imshow("new", resized_cropped)
-    cv2.imwrite(save_path, cropped)
+    cv2.imshow(save_path, resized_cropped)
+    # cv2.imwrite(save_path, cropped)
+    save_path_suffix = os.path.splitext(image_path)[-1]
+    """Solver Chinese path"""
+    cv2.imencode(save_path_suffix, cropped)[1].tofile(save_path)
+
 
 
